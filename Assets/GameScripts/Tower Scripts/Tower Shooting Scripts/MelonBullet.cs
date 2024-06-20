0b5216f68476175af9ed7b17c87d9fe9b5cc8291
arsen15 <ukon1511@gmail.com>
2024-06-19 18:22:58 -0700

    Implemented tower targetting and shooting, but bullets spawn offset to the left. I fixed this by offsetting the fire point position.

---
+++ Assets/GameScripts/Tower Scripts/Tower Shooting Scripts/MelonBullet.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MelonBullet : MonoBehaviour
+{
+    private Transform target;
+
+    public float speed = 70f;
+
+    public GameObject bulletImpactEffect;
+
+    public void Seek(Transform _target)
+    {
+        target = _target;
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // Direction in which the bullet needs to point in
+        Vector3 direction = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        // if distance between bullet and target is less than the distance that we move this frame(overshooting)
+        if (direction.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(direction.normalized * distanceThisFrame, Space.World);
+
+    }
+
+    void HitTarget()
+    {
+        Debug.Log("We hit something");
+        GameObject effectInstance = (GameObject)Instantiate(bulletImpactEffect, transform.position, transform.rotation);
+        Destroy(effectInstance, 2f);
+        Destroy(gameObject);
+    }
+}
