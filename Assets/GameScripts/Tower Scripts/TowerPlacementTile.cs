4f2e58d4dc31e62171320b5f66b1609967d3eeaa
arsen15 <ukon1511@gmail.com>
2024-07-10 17:08:00 -0700

    Implemented the upgrade functionality. Updated the shop and upgrade UI.

--- Assets/GameScripts/Tower Scripts/TowerPlacementTile.cs
+++ Assets/GameScripts/Tower Scripts/TowerPlacementTile.cs
@@ -13,12 +13,17 @@ public class TowerPlacementTile : MonoBehaviour
     private Renderer rend;
 
     [HideInInspector]
-    private GameObject turret;
+    private GameObject tower;
 
     [HideInInspector]
     public TowerBlueprint towerBlueprint;
 
+    [HideInInspector]
+    public bool isUpgraded = false;
+
     TowerBuildManager buildManager;
+
+    
 
     private void Start()
     {
@@ -30,7 +35,7 @@ public class TowerPlacementTile : MonoBehaviour
     private void OnMouseDown()
     {
         // If there is tower to build, build it
-        if (buildManager.CanBuild && turret == null)
+        if (buildManager.CanBuild && tower == null)
         {
             
             BuildTower(buildManager.GetTowerToBuild());
@@ -39,7 +44,7 @@ public class TowerPlacementTile : MonoBehaviour
         }
 
         // If there is no tower to build, select this node if it already has a tower
-        if (turret != null)
+        if (tower != null)
         {
             buildManager.SelectNode(this);
         }
@@ -58,9 +63,30 @@ public class TowerPlacementTile : MonoBehaviour
         }
         PlayerStats.Money -= blueprint.cost;
         GameObject _turret = (GameObject)Instantiate(blueprint.prefab, transform.position, transform.rotation);
-        turret = _turret;
+        tower = _turret;
         towerBlueprint = blueprint;
 
+    }
+
+    public void UpgradeTower()
+    {
+        if (PlayerStats.Money < towerBlueprint.upgradeCost)
+        {
+            Debug.Log("Need more money for the upgrade!");
+            return;
+        }
+        PlayerStats.Money -= towerBlueprint.upgradeCost;
+
+        // Remove old tower
+        Destroy(tower);
+
+        // Build the upgraded tower
+        GameObject _turret = (GameObject)Instantiate(towerBlueprint.upgradedPrefab, transform.position, transform.rotation);
+        tower = _turret;
+
+        isUpgraded = true;
+
+        Debug.Log("Tower Upgraded");
     }
     private void OnMouseEnter()
     {
