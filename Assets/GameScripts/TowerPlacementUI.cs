4d17c6f316c04e9e133ebe612ca5cd7b390ea409
nepneko <mazelin51@gmail.com>
2024-07-30 15:43:44 -0700

    button color change script

--- Assets/GameScripts/TowerPlacementUI.cs
+++ Assets/GameScripts/TowerPlacementUI.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,60 +7,52 @@ public class TowerPlacementUI : MonoBehaviour
 {
     public Button towerButton;
     public Image towerImage;
-    public Color selectedColor = Color.gray;  
-    private Color originalColor;  
-
-    private bool isTowerSelected = false;  
+    public Color selectedColor = Color.gray;
+    private Color originalColor;
+    private bool isPlacingTower = false;
 
     private void Start()
     {
+        originalColor = towerImage.color;
+        towerButton.onClick.AddListener(OnTowerButtonClick);
+    }
 
-        if (towerImage != null)
+    private void Update()
+    {
+        // 如果正在放置塔，且塔放置完成
+        if (isPlacingTower && !TowerBuildManager.instance.CanBuild)
         {
-            originalColor = towerImage.color;
-        }
-
-
-        if (towerButton != null)
-        {
-            towerButton.onClick.AddListener(OnTowerButtonClick);
+            towerImage.color = originalColor;
+            isPlacingTower = false;
         }
     }
 
     private void OnTowerButtonClick()
     {
-        if (isTowerSelected)
-        {
-  
-            return;
-        }
+        towerImage.color = selectedColor;
+        isPlacingTower = true;
 
+        // 假设点击按钮后设置一个要建造的塔
+        TowerBuildManager.instance.SetTowerToBuild(TowerBuildManager.instance.GetTowerToBuild());
 
-        if (towerImage != null)
-        {
-            towerImage.color = selectedColor;
-        }
-
-        isTowerSelected = true;
-
-    
-        PlaceTower();
+        // 启动一个协程来检查塔是否已放置
+        StartCoroutine(CheckTowerPlaced());
     }
 
-    private void PlaceTower()
+    private IEnumerator CheckTowerPlaced()
     {
+        // 等待一小段时间来模拟塔放置过程
+        yield return new WaitForSeconds(0.1f);
 
-        Invoke("OnTowerPlaced", 2.0f); 
-    }
-
-    private void OnTowerPlaced()
-    {
-
-        if (towerImage != null)
+        // 持续检查塔是否放置完成
+        while (isPlacingTower)
         {
-            towerImage.color = originalColor;
+            if (!TowerBuildManager.instance.CanBuild)
+            {
+                towerImage.color = originalColor;
+                isPlacingTower = false;
+            }
+            yield return null;
         }
-
-        isTowerSelected = false;
     }
 }
